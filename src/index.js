d05d85a85452d791beec0f69b6dfe3c72fdbfc9e
Zoreslav Goral <zgoral@softserveinc.com>
2017-11-28 13:07:23 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default class {
+
+    constructor() {
+        this.data = 'test';
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+const libraryName = "loglevel-plugin-server";
+const outputFile = libraryName + ".min.js";
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        path: path.join(__dirname, "lib"),
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, "node_modules")
+        ]
+    },
+    plugins: [
+        new uglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
