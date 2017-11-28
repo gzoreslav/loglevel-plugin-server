eca0c552bb84754b529516c51bc3aee0b9ebe313
Zoreslav Goral <zgoral@softserveinc.com>
2017-11-28 13:50:48 +0200

    first worked version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,70 @@
-export default class {
+export const defaultJSONFormat = ({message, methodName, logLevel, loggerName}) => {
+    return JSON.stringify({
+        message: message,
+        level: {
+            label: (methodName || '').toUpperCase(),
+            value: logLevel
+        },
+        logger: loggerName || '',
+        timestamp: new Date().toISOString()
+    });
+};
 
-    constructor() {
-        this.data = 'test';
+export default (logger, options) => {
+
+    if (!logger || !logger.methodFactory) {
+        throw new Error('loglevel instance has to be specified in order to be extended');
     }
-}
+
+    let pluginLogger = logger,
+        pluginUrl = options && options.url || 'http://localhost:8080/log',
+        pluginCallOriginal = options && options.callOriginal || false,
+        pluginLevel = options && options.level || 'trace',
+        pluginFormat = options && options.format || defaultJSONFormat,
+        pluginOriginalFactory = pluginLogger.methodFactory,
+        pluginSendQueue = [],
+        pluginIsSending = false;
+
+    pluginLogger.methodFactory = (methodName, logLevel, loggerName) => {
+        let rawMethod = pluginOriginalFactory(methodName, logLevel, loggerName);
+
+        return (message) => {
+            const formattedMessage = pluginFormat({
+                message: message,
+                methodName: methodName,
+                logLevel: logLevel,
+                loggerName: loggerName
+            });
+
+            if (pluginCallOriginal) {
+                rawMethod(message);
+            }
+
+            pluginSendQueue.push(formattedMessage);
+            sendNextMessage();
+        };
+    };
+
+    pluginLogger.setLevel(pluginLevel);
+
+    const sendNextMessage = () => {
+        if (!pluginSendQueue.length || pluginIsSending) {
+            return null;
+        }
+
+        pluginIsSending = true;
+
+        let msg = pluginSendQueue.shift(),
+            req = new XMLHttpRequest();
+
+        req.open('POST', pluginUrl, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.onreadystatechange = () => {
+            if (req.readyState === 4) {
+                pluginIsSending = false;
+                setTimeout(sendNextMessage, 0);
+            }
+        };
+        req.send(msg);
+    };
+};
